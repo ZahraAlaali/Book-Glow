0f1c9d724871d2fb1778bdb916b8a32f4b3ee01f
ZahraAlaali <zooz.alaali03@icloud.com>
2025-10-02 11:15:54 +0300

    server+db

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require("express")
+const app = express()
+require("dotenv").config()
+
+// Database Configuration
+require("./config/db")
+
+//Port Configuration
+const PORT = process.env.PORT ? process.env.PORT : "3000"
+
+// Require/Use Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+const methodOverride = require("method-override")
+app.use(methodOverride("_method"))
+
+const morgan = require("morgan")
+app.use(morgan("dev"))
+
+// const isSignedIn = require("./Middlewares/isSignedIn")
+
+const session = require("express-session")
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+)
+
+// const passUser = require("./Middlewares/pass-user")
+// app.use(passUser)
+
+// Auth Router
+const authRouter = require("./routes/authRouter")
+app.use("/auth", authRouter)
+
+// User Router
+const userRouter = require("./routes/userRouter")
+app.use("/users", isSignedIn, userRouter)
+
+
+
+app.get("/", (req, res) => {
+  res.render("index.ejs")
+})
+
+app.listen(PORT, () => {
+  console.log(`Running Server on Port ${PORT} . . . `)
+})
