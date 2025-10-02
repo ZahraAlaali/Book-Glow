d8cc747e9772426a42621b7115b2ff519e3a8c06
shooqbinzaiman <shooqshowqi@gmail.com>
2025-10-02 13:01:31 +0300

    signin

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,2 +1,20 @@
 const bcrypt= require("bcrypt")
 const User= require("../models/User.js")
+
+exports.auth_signin_get =  async (req, res) => {
+  res.render("auth/sign-in.ejs")
+}
+
+exports.auth_signin_post = async (req, res) => {
+  const userInDatabase = await User.findOne({ username: req.body.username});
+  if(!userInDatabase){
+    return res.send("Login failed. Please try again later...")
+}
+
+  req.session.user = {
+    username: userInDatabase.username,
+    _id: userInDatabase._id
+  };
+
+  res.redirect("/");
+}
