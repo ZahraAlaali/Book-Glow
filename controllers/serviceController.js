f5eb18b27ab64748b8491b9b72596ea0bb5f5d61
ZahraAlaali <zooz.alaali03@icloud.com>
2025-10-04 21:46:02 +0300

    addService post

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -5,3 +5,12 @@ exports.addService_get = async (req, res) => {
   const salon = { _id: "68de6ea59b85efa19f5de528" }
   res.render("services/create.ejs", { salon })
 }
+// Create salon's services
+exports.addService = async (req, res) => {
+  const salonInDatabase = await Salon.findById(req.params.salonId)
+  if (salonInDatabase && salonInDatabase.ownerId.equals(req.session.user._id)) {
+    req.body.salonId = req.params.salonId
+    const service = await Service.create(req.body)
+    res.send({ service })
+  } else res.send("error")
+}
