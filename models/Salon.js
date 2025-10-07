4d57b8b9d033aae752d06dfd37e676ffbd980781
Fatima-Zaid <fatimaameer957@gmail.com>
2025-10-07 16:11:44 +0300

    add salon img

--- models/Salon.js
+++ models/Salon.js
@@ -6,9 +6,19 @@ const salonSchema = new mongoose.Schema(
     location: String,
     phone: Number,
     ownerId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    salonImg: {
+      name:{
+        type: String,
+      },
+      image:{
+        data: Buffer,
+        contentType: String
+      }
   },
+},
   { timestamps: true }
 )
+
 
 const Salon = mongoose.model("Salon", salonSchema)
 
